5153bb9d1acf6d2cdb1259bacb19583eb5548fc4
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-09 21:04:58 +0900

    First Commit

---
+++ Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/stack.py
@@ -0,0 +1,19 @@
+"""
+Python Data Structures - A Game-Based Approach
+Stack class
+Robin Andrews - https://compucademy.net/
+"""
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
